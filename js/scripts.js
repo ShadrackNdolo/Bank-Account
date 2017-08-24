22e35234f5e1eab0ea3ec6337703e41479d7e8ef
Ndolo S <shadrack.winters@outlook.com>
2017-08-24 14:46:52 +0300

    Editing the HTML and creating a new Customer account. Replacing the id 'name' with 'customerName'in the input field for customer name within the HTML. Adding an event listener for the button 'createNewAccount' in the js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
 // Back-end
-function BankAccount(name,initialDeposit) {
+
+// Constructor for a customer's bank account
+function BankAccount(name,balance) {
   this.name = name;
-  this.initialDeposit = initialDeposit;
+  this.balance = balance;
 }
 
-// Front-nd
+// Prototype
+
+BankAccount.prototype.deposit = function () {
+  return 
+}
+// Front-end
 $(document).ready(function() {
-  
+  $('#createNewAccount').submit(function (event) {
+    event.preventDefault();
+
+    var inputtedName = $('input#customerName').val();
+    var inputtedDeposit = parseInt($('input#balance').val());
+
+    var newCustomer = new BankAccount(inputtedName , balance);
+    // Test
+    console.log(newCustomer);
+
+    $('#currentBalance').text(inputtedDeposit);
+    });
 });
