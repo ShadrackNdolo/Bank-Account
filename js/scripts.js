683d4a9b24b2826577e2d292c150a0bc5d470989
Ndolo S <shadrack.winters@outlook.com>
2017-08-24 17:36:19 +0300

    Successfully working banking system.
    Adding a p tag in the current balance section  of the HTML.
    Including the BankAccount prototype for deposit and withdrawal.
    Setting up submit functions for the Create Account,Deposit and Withdrawal.

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,75 @@ function BankAccount(name,balance) {
 
 // Prototype
 
-BankAccount.prototype.deposit = function () {
-  return 
+// Check if customer has sufficient funds
+// BankAccount.prototype.sufficientAmount = function (amount) {
+//   const sufficientAmount = amount > 0;
+//   if (!sufficientAmount){
+//     console.log("Insufficient Amount");
+//     return false;
+//   }
+//   return true;
+//   console.log("Sufficient Amount");
+// }
+
+// Making a deposit
+BankAccount.prototype.deposit = function (amount) {
+    this.balance += amount;
+    return this.balance;
 }
+
+// Making a withdrawal
+BankAccount.prototype.withdrawal = function (amount) {
+  this.balance -= amount;
+  return this.balance;
+}
+
+// Reset input fields
+function resetFields() {
+  $('input#customerName').val("");
+  $('input#initialDeposit').val("");
+  $('input#enterDepositAmount').val("");
+  $('input#enterWithdrawAmount').val("");
+}
+
 // Front-end
 $(document).ready(function() {
   $('#createNewAccount').submit(function (event) {
     event.preventDefault();
 
     var inputtedName = $('input#customerName').val();
-    var inputtedDeposit = parseInt($('input#balance').val());
+    var inputtedDeposit = parseFloat($('input#initialDeposit').val());
 
-    var newCustomer = new BankAccount(inputtedName , balance);
+    var newCustomer = new BankAccount(inputtedName, inputtedDeposit);
     // Test
     console.log(newCustomer);
+    $('#currentBalance').append('<p>'+ newCustomer.balance + '</p>');
 
-    $('#currentBalance').text(inputtedDeposit);
+
+    $("form#depositAmount").submit(function(event) {
+      event.preventDefault();
+
+      $('#currentBalance').empty();
+      var depositAdded = parseFloat($('input#enterDepositAmount').val());
+      console.log(newCustomer.deposit(depositAdded));
+      console.log(newCustomer.balance);
+      $('#currentBalance').append('<p>'+ newCustomer.balance + '</p>');
+
+      resetFields();
     });
+
+    $("form#withdrawAmount").submit(function(event) {
+      event.preventDefault();
+
+      $('#currentBalance').empty();
+      var withdrawnAmount = parseFloat($('input#enterWithdrawAmount').val());
+      console.log(newCustomer.withdrawal(withdrawnAmount));
+      console.log(newCustomer.balance);
+      $('#currentBalance').append('<p>'+ newCustomer.balance + '</p>');
+
+      resetFields();
+    })
+    resetFields();
+  });
+
 });
